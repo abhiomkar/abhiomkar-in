ea015724c36188d49102a3333a34e8dfcecbf4f8
Abhinay Omkar <abhiomkar@google.com>
2016-03-28 12:56:58 +0530

    Homepage changes

--- app/app.py
+++ app/app.py
@@ -1,5 +1,9 @@
+import markdown
+import simplejson as json
+
 from flask import Flask, send_from_directory
 from flask import render_template
+from flask import Markup
 
 import settings
 
@@ -7,7 +11,15 @@ app = Flask(__name__, template_folder="./")
 
 @app.route("/")
 def home():
-    return render_template("home/home.html", name="home")
+    name = "home"
+    content = Markup(markdown.markdown(open(settings.APP_PATH + "/home/home.md").read()))
+    return render_template("home/home.html", **locals())
+
+@app.route("/projects")
+def projects():
+    name = "projects"
+    repoList = json.loads(open(settings.APP_PATH + "/projects/repos.json").read())
+    return render_template("projects/projects.html", **locals())
 
 # @app.route('/public/<path:path>')
 # def serve_static(path):
--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,7 @@
 import os
 
+APP_PATH = os.path.dirname(__file__)
+
 if str(os.getenv("DEBUG")).lower() in ["on", "1", "true"]:
     DEBUG = True
 else:
