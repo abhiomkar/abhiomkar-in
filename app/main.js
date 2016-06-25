df86c459675783554a1179683d6fde9f8ea5b47d
Abhinay Omkar <abhiomkar@gmail.com>
2016-06-26 00:31:23 +0530

    lazy loading images

--- app/main.js
+++ app/main.js
@@ -2,4 +2,20 @@ import './home/home.js';
 import './projects/projects.js';
 import './photography/photography.js';
 
-console.log('Init');
+class Main {
+  constructor () {
+    this.lazyLoadImages();
+  }
+
+  lazyLoadImages () {
+    [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.onload = function() {
+        img.removeAttribute('data-src');
+      };
+    });
+  }
+}
+
+window.addEventListener('load', () => new Main());
+
--- app/photography/photography.js
+++ app/photography/photography.js
@@ -1,3 +1,5 @@
 import './photography.css';
 
-console.log("Photography");
+const PhotographyController = function() {
+
+};
