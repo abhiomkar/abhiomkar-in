25afe032d12138390f0fa54790ebd59e1df8df5a
Abhinay Omkar <abhiomkar@gmail.com>
2016-06-26 11:34:32 +0530

    photography gallery navigation with deep link

--- app/photography/photography.js
+++ app/photography/photography.js
@@ -1,5 +1,55 @@
 import './photography.css';
 
-const PhotographyController = function() {
+class Photography {
+  constructor () {
+    this.$el = document.querySelector('.photography-container');
+    this.$galleryNav = this.$el.querySelector('.category-nav');
+    this.$galleryList = this.$el.querySelector('.gallery-list');
 
-};
+    this.navigateToGallery = this.navigateToGallery.bind(this);
+    this.showGallery = this.showGallery.bind(this);
+    this.addEventListeners();
+
+    // shows current gallery based on the url hash
+    this.showGallery();
+  }
+
+  addEventListeners () {
+    this.$galleryNav.addEventListener('click', this.navigateToGallery);
+    if (window.location.pathname === '/photography') {
+      window.addEventListener('hashchange', this.showGallery);
+    }
+  }
+
+  navigateToGallery (ev) {
+    const galleryId = ev.target.getAttribute('data-gallery-id');
+    window.location.hash = galleryId;
+  }
+
+  showGallery () {
+    let galleryId = window.location.hash;
+    if (galleryId.startsWith('#')) {
+      galleryId = galleryId.slice(1);
+    }
+
+    // select gallery nav link
+    const $selectedNavLink = this.$galleryNav.querySelector(`[data-gallery-id="${galleryId}"`);
+    // do nothing if the gallery with the id doesn't exist including no hash.
+    if (!$selectedNavLink) {
+      return;
+    }
+    this.$galleryNav.querySelectorAll('.category-link').forEach((linkEl) => {
+      linkEl.classList.remove('is-selected');
+    });
+    $selectedNavLink.classList.add('is-selected');
+
+    // show target gallery
+    this.$galleryList.querySelectorAll('.gallery').forEach((galleryEl) => {
+      galleryEl.classList.remove('is-visible');
+    });
+    const $selectedGallery = this.$galleryList.querySelector(`[data-gallery-id="${galleryId}"`);
+    $selectedGallery.classList.add('is-visible');
+  }
+}
+
+window.addEventListener('load', () => new Photography());
